754e827a7113f12b5ddf9717874f20d30f861213
Mohamed Yasser <mohamed3laily@gmail.com>
2026-01-05 23:56:51 +0200

    fix database connection

--- src/modules/db/drizzle.service.ts
+++ src/modules/db/drizzle.service.ts
@@ -18,6 +18,9 @@ export class DrizzleService implements OnApplicationBootstrap {
 
     const pool = new Pool({
       connectionString: databaseUrl,
+      ssl: {
+        rejectUnauthorized: false,
+      },
     });
 
     this.db = drizzle(pool, {
