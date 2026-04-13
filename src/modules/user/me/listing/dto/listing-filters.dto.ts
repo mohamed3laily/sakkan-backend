b7656a4e365352cbfd4768a82bca90825a0be516
Mohamed Yasser <mohamed.alaily@educatly.com>
2026-04-13 21:42:42 +0200

    fixed validation in creating listing

--- src/modules/user/me/listing/dto/listing-filters.dto.ts
+++ src/modules/user/me/listing/dto/listing-filters.dto.ts
@@ -28,6 +28,7 @@ export class ListingFiltersDto {
 
   @IsOptional()
   @IsArray()
+  @Transform(({ value }) => (Array.isArray(value) ? value : [value]))
   @Type(() => Number)
   areaIds?: number[];
 
